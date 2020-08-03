36e401263509ef909f2764b02c055b1e40e1313b
Jon <291976422@qq.com>
2020-08-03 23:37:23 +0800

    # feat: 无重复字符最长子串

---
+++ 无重复字符的最长子串/lengthOfLongestSubstring.py
@@ -0,0 +1,14 @@
+def lengthOfLongestSubstring(self, s: str):
+    occ = set()
+    n = len(s)
+    rk, ans = -1, 0
+    for i in range(n):
+        if i != 0:
+            occ.remove(s[i - 1])
+        while rk + 1 < n and s[rk + 1] not in occ:
+            occ.add(s[rk + 1])
+            rk += 1
+        ans = max(ans, rk + 1 - i)
+    return ans
+
+print(lengthOfLongestSubstring("abcabcbb", "abcabcbb"))
