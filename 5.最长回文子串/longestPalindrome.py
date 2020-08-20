b03b5361558070db8a35d0fc5d155f17ded215d1
Jon <291976422@qq.com>
2020-08-20 23:31:11 +0800

    # feat: 5. 最长回文子串  动态规划1

--- 5.最长回文子串/longestPalindrome.py
+++ 5.最长回文子串/longestPalindrome.py
@@ -28,26 +28,31 @@ class Solution:
 
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
+        if n < 2:
+            return s
+
         dp = [[False] * n for _ in range(n)]
-        ans = ""
-        # 枚举子串的长度 l + 1
-        for l in range(n):
-            # 枚举子串的起始位置 i，这样可以通过 j=i+l 得到子串的结束位置
-            for i in range(n):
-                j = i + l
-                if j == len(s):
-                    break
-                if l == 0:
-                    dp[i][j] = True
-                elif l == 1:
-                    dp[i][j] = (s[i] == s[j])
+        ans = s[0]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for j in range(1, n):
+            for i in range(0, j):
+                if s[i] == s[j]:
+                    if j - i < 3:
+                        dp[i][j] = True
+                    else:
+                        dp[i][j] = dp[i + 1][j - 1]
                 else:
-                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
-                if dp[i][j] and l + 1 > len(ans):
-                    ans = s[i: j + 1]
+                    dp[i][j] = False
+                if dp[i][j]:
+                    if j - i + 1 > len(ans):
+                        ans = s[i: j + 1]
         return ans
 
 
+
 s = Solution()
-lll = 'abcda'
+lll = 'aaaa'
 print(s.longestPalindrome(lll))
